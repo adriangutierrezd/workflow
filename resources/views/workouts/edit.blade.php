98475b3a4fabb7714b4e7ea6e818c05033aebddf
adriangutierrezd <adriangutierrezdelaosa@gmail.com>
2023-12-10 17:45:15 +0100

    Ring en btn

--- resources/views/workouts/edit.blade.php
+++ resources/views/workouts/edit.blade.php
@@ -18,10 +18,10 @@
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button
-                                class="inline-flex items-center px-3 py-2 border border-transparent
-                                text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400
-                                bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300
-                                focus:outline-none transition ease-in-out duration-150"
+                                class="relative z-10 block p-2 text-gray-700 bg-white border
+                                border-transparent rounded-md dark:text-white focus:border-blue-500
+                                focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300
+                                dark:focus:ring-blue-400 focus:ring dark:bg-gray-800 focus:outline-none"
                             >
                                 <x-chevron-down-icon/>
                             </button>
